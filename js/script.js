e5aa04dd89f8c2d6b40883ae072116c0ba63d168
HanifDwiA <hanifdwi1995@gmail.com>
2023-11-23 15:19:44 +0700

    bismillah

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 // Sticky Navbar
-
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
 
 var slideIndex = 1;
 showDivs(slideIndex);
